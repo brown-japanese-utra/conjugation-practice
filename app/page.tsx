0a9ba7f0549f8cd011eabb4d21e01a3fdf6fe333
anglearzola <41270900+anglearzola@users.noreply.github.com>
2024-12-03 15:56:02 -0500

    cards instead of buttons

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,64 @@
 "use client";
 
-import { Button } from "@mantine/core";
+import {
+  Button,
+  Card,
+  Group,
+  Image,
+  Text,
+  Badge,
+  Center,
+  Container,
+} from "@mantine/core";
 import Link from "next/link";
 
 export default function HomePage() {
   return (
-    <Button.Group>
-      <Link href={{ pathname: "/story", query: { frame: 0 } }}>
-        <Button>Story 1</Button>
-      </Link>
-      <Link href={{ pathname: "/story", query: { frame: 15 } }}>
-        <Button>Story 2</Button>
-      </Link>
-      <Link href={{ pathname: "/story", query: { frame: 30 } }}>
-        <Button>Story 3</Button>
-      </Link>
-    </Button.Group>
+    <Center>
+      <Container size="lg">
+        <Link
+          href={{ pathname: "/story", query: { frame: 0 } }}
+          style={{ textDecoration: "none", width: "100%" }}
+        >
+          <Card shadow="sm" padding="xl">
+            <Card.Section>
+              <Image src="frames/frame-1-5.jpg" h={160} alt="No way!" />
+            </Card.Section>
+
+            <Text fw={500} size="lg" mt="md" td="none">
+              Bobo&apos;s Day at School
+            </Text>
+          </Card>
+        </Link>
+        <Link
+          href={{ pathname: "/story", query: { frame: 15 } }}
+          style={{ textDecoration: "none" }}
+        >
+          <Card shadow="sm" padding="xl">
+            <Card.Section>
+              <Image src="frames/frame-2-3.jpg" h={160} alt="No way!" />
+            </Card.Section>
+
+            <Text fw={500} size="lg" mt="md">
+              Bobo Plays a Video Game
+            </Text>
+          </Card>
+        </Link>
+        <Link
+          href={{ pathname: "/story", query: { frame: 30 } }}
+          style={{ textDecoration: "none" }}
+        >
+          <Card shadow="sm" padding="xl">
+            <Card.Section>
+              <Image src="frames/frame-3-10.jpg" h={160} alt="No way!" />
+            </Card.Section>
+
+            <Text fw={500} size="lg" mt="md">
+              Bobo Does a Relay Race
+            </Text>
+          </Card>
+        </Link>
+      </Container>
+    </Center>
   );
 }
