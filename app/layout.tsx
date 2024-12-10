1cdeda2d2ec839d990db4144645e8bffd56a5329
DarkShinyGiratina <ross_williams@brown.edu>
2024-12-10 14:46:13 -0500

    default dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,23 +5,19 @@ import { theme } from "../theme";
 
 export const metadata = {
   title: "日本語のコンジュゲーションのゲーム",
-  description:
-    "Game for conjugating intransitive and transitive verbs in Japanese",
+  description: "Game for conjugating intransitive and transitive verbs in Japanese",
 };
 
 export default function RootLayout({ children }: { children: any }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <ColorSchemeScript defaultColorScheme="auto" />
+        <ColorSchemeScript defaultColorScheme="dark" />
         <link rel="shortcut icon" href="/favicon.svg" />
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
-        />
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no" />
       </head>
       <body>
-        <MantineProvider theme={theme} defaultColorScheme="auto">
+        <MantineProvider theme={theme} defaultColorScheme="dark">
           {children}
         </MantineProvider>
       </body>
